955cb2aa51d0c0e5ff1e6635144e6833c1473f2e
Михаил Луцко <lutsko86@gmail.com>
2024-09-30 10:33:58 +0300

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using System.Windows;
+
+namespace HW9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Обработка нажатия первой кнопки для разделения строки на слова
+        /// </summary>
+        private void OnSplitWordsButtonClick(object sender, RoutedEventArgs e)
+        {
+            string inputText = InputTextBoxSplitWords.Text;
+
+            WordsListBox.Items.Clear();
+
+            string[] words = inputText.Split(' ');
+
+            foreach (string word in words)
+            {
+                WordsListBox.Items.Add(word);
+            }
+        }
+
+        /// <summary>
+        /// Обработка нажатия второй кнопки для перестановки слов
+        /// </summary>
+        private void OnReverseWordsButtonClick(object sender, RoutedEventArgs e)
+        {
+            string inputText = InputTextBoxReverseWords.Text;
+
+            string[] words = inputText.Split(' ');
+
+            string reversedSentence = string.Join(" ", words.Reverse());
+
+            ReversedSentenceLabel.Content = reversedSentence;
+        }
+    }
+}
